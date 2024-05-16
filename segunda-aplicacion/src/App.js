15352ee193aff0abaac852adc5de93dffc1fee5c
Paolo Gamarra <paologamarra10@gmail.com>
2024-05-16 19:02:32 -0300

    video8_Manejo de eventos

--- segunda-aplicacion/src/App.js
+++ segunda-aplicacion/src/App.js
@@ -1,13 +1,41 @@
 import './App.css';
+import React, { useState } from "react";
 
 //componente
 function App() {
-  
+
   const elemento = <h1 className="centrar-titulo">¡Hola Mundo!</h1>;
-  const elemento2 = suma(7, 9);
+  const elemento2 = <h2>{suma(7, 9)}</h2>; {/*el codigo js debe ir entre llaves*/ }
+
+  //definir el estado para almacenar el estado de la suma
+  const [resultado, setResultado] = useState(null);
+  const botonPulsado = () => {
+    const result = suma(7, 5);
+    setResultado(result);
+  }
 
   //crear nodo padre para devolver los 2 nodos hijos 
-  return <div><div>{elemento}</div><div>Suma: {elemento2}</div></div>
+  return (
+    <div>
+      <button
+        style={{ marginTop: "10px", marginLeft: "10px" }} /*estilo inline*/
+        onClick={botonPulsado}
+      >
+        Pulsar
+      </button>
+
+      <div>
+        {elemento}
+      </div>
+
+      <div> {/*condicional*/}
+        {resultado != null && 
+          <h2>
+            Resultado: {resultado}
+          </h2>}
+      </div>
+    </div>
+  )
 }
 
 //no se considera componente
