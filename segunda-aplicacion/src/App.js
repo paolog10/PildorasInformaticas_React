343ea22e5aec75bbb452732ebf2bb26e521068b8
Paolo Gamarra <paologamarra10@gmail.com>
2024-05-26 19:22:27 -0300

    Vídeo9_useState vs useRef

--- segunda-aplicacion/src/App.js
+++ segunda-aplicacion/src/App.js
@@ -1,46 +1,41 @@
 import './App.css';
-import React, { useState } from "react";
+import React, { useRef } from "react";
+import video from './assets/Figurines-tv - Article - Lavis-glacis (acrylique).mp4'
 
-//componente
+//useRef se puede acceder a un elemento del DOM
 function App() {
 
-  const elemento = <h1 className="centrar-titulo">¡Hola Mundo!</h1>;
-  const elemento2 = <h2>{suma(7, 9)}</h2>; {/*el codigo js debe ir entre llaves*/ }
-
-  //definir el estado para almacenar el estado de la suma
-  const [resultado, setResultado] = useState(null);
-  const botonPulsado = () => {
-    const result = suma(7, 5);
-    setResultado(result);
+  const videoRef = useRef(null);
+  const videoPlay = () => {
+    videoRef.current.play();
+  };
+  const videoPause = () => {
+    videoRef.current.pause();
   }
 
-  //crear nodo padre para devolver los 2 nodos hijos 
   return (
     <div>
-      <button
-        style={{ marginTop: "10px", marginLeft: "10px" }} /*estilo inline*/
-        onClick={botonPulsado}
+      <video
+        width={400}
+        ref={videoRef}  
       >
-        Pulsar
-      </button>
-
+        <source
+          src={video}
+          type='video/mp4'
+        >
+        </source>
+      </video>
       <div>
-        {elemento}
-      </div>
-
-      <div> {/*condicional*/}
-        {resultado != null && 
-          <h2>
-            Resultado: {resultado}
-          </h2>}
+        <button onClick={videoPlay}>Play</button>
+        <button onClick={videoPause}>Pause</button>
       </div>
     </div>
-  )
+
+  );
 }
 
-//no se considera componente
-function suma(a, b) {
-  return a + b
-}
-
+/*
+useState: gestionar el estado de un componente, SI necesita renderizar, Es reactivo
+useRef: para cambiar su estado NO necesita renderizar, No es reactivo
+*/
 export default App;
