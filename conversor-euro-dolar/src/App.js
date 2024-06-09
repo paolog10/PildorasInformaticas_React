fdfb932ce6589a97b1aa331bbf4b0eab9bbb8d75
Paolo Gamarra <paologamarra10@gmail.com>
2024-06-09 16:53:02 -0300

    video11_Llamada asíncrona para convertir euro-dolar

--- conversor-euro-dolar/src/App.js
+++ conversor-euro-dolar/src/App.js
@@ -1,23 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
-import {useRef} from 'react'
+import {useRef, useState, useEffect} from 'react'
 
 function App() {
-  //no se necesita renderizar elementos, ni actualizar la pagina. Usar useRef
+  
+  const [valorCambio, setValorCambio] = useState(null)
   
   //se almacena el valor que el cliente ingresa en el cuadro de texto
   const euroRef = useRef();
-
+  
   //almacena el resultado, no se necesita renderizar el componente 
   const resultafoRef = useRef();
+  
+  //Programación Asíncrona
+  useEffect(() => {
+    const llamaApiCambio = async() => {
+      try {
+        const respuesta = await fetch("https://v6.exchangerate-api.com/v6/dd19051bc803b874d9859518/latest/EUR");
+        const datos = await respuesta.json();
 
+        console.log(datos);
+        
+        setValorCambio(datos.conversion_rates.USD);
+
+      } catch (error) {
+        console.error("Error al acceder a la API: ", error)
+      }
+    };
+    
+    llamaApiCambio();
+  }, []);
+  
   //funcion que calcula la conversion
   const calcularConversion = () => {
     //valor actual de lo que ingresó el cliente
     const eurosValor=parseFloat(euroRef.current.value);
     
     //almacena el resultado de la conversión
-    const dolares = eurosValor * 1.08    //02/06/2024 1dolar=1.08euros
+    const dolares = eurosValor * valorCambio;
 
     //mostrar el resultado
     resultafoRef.current.innerHTML =  dolares.toFixed(2) + 'U$S'
